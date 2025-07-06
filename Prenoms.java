4a899025370223a422743be4687f6e15c7ba7450
kompane olivia <kompanejosie@gmail.com>
2025-07-06 15:34:13 +0200

    Ajout du fichier Prenoms.java avec tablleaux de prenoms

---
+++ Prenoms.java
@@ -0,0 +1,9 @@
+public class Prenoms {
+    public static void main(String[] args) {
+        String[] prenoms = {"Olivia","Inona","Lucas","Emma","Marie"};
+        System.out.println("Liste des prenoms :");
+        for (int i = 0; i< prenoms.length; i++){
+            System.out.println("prenom a l'index " + i + ": " + prenoms[i] );
+        }
+    }
+}
