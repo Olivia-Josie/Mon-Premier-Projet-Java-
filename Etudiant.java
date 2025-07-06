4aa694a956d668592c929fca06020f92893e6943
kompane olivia <kompanejosie@gmail.com>
2025-07-06 14:42:19 +0200

    site apprentissage

---
+++ Etudiant.java
@@ -0,0 +1,12 @@
+public class Etudiant {
+    public static void main(String[] args) {
+        String nom = "Olivia";
+        int age = 21;
+        String filiere = "Informatique";
+        double moyenne = 14.5;
+        System.err.println("Nom : " +nom);
+        System.out.println("Age : "+ age + "ans");
+        System.out.println("Filiere : "+ filiere);
+        System.out.println("Moyenne :" + moyenne +"/20");
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double taille = 1.65;
+        boolean estEtudiante = true;
+        String nom = "Olivia";
+        System.out.println("Nom : "+ nom);
+        System.out.println("Age : "+ age +"ans");
+        System.out.println("Taille : "+ taille +"m");
+        System.out.println("Etudiante ? "+ estEtudiante);
+    }
+}
