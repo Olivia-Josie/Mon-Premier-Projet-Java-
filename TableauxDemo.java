4de7756aa911c454e41693514f4ff41355bd766a
kompane olivia <kompanejosie@gmail.com>
2025-07-06 15:20:47 +0200

    tableaux

---
+++ TableauxDemo.java
@@ -0,0 +1,9 @@
+public class TableauxDemo {
+    public static void main(String[] args) {
+        int[] nombres ={2,4,6,8,10};
+        System.out.println("Voici les elements du tableau :");
+        for (int i = 0; i< nombres.length; i++){
+        System.out.println("Element a l'index " + i + " :" + nombres[i]);
+        }
+    }
+}
